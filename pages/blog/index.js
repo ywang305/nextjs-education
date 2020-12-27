c240f1bd8eceaf4b2c376a2cce014a4ea09417ec
yao <ywang305@syr.edu>
2020-12-27 14:40:43 -0500

    noth

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,23 +1,38 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import ListItem from '@material-ui/core/ListItem';
 import axios from 'axios';
 
 const uri = 'https://raw.githubusercontent.com/ywang305/blog/main/blogs.md';
 
 async function getBlogMd(onComplete) {
     const resp = await axios.get(uri);
-    onComplete?.(resp.data);
+    onComplete?.(resp?.data);
 }
 
 export default function BlogPage() {
-    const [md, setMd] = useState('');
+    const [md, setMd] = useState(null);
     useEffect(() => {
         getBlogMd(setMd);
     }, []);
 
     return (
         <div>
-            <ReactMarkdown linkTarget='_blank'>{md}</ReactMarkdown>
+            <ReactMarkdown renderers={{ link: Link }}>{md}</ReactMarkdown>
         </div>
     );
 }
+
+const Link = props => {
+    const {
+        node: { children },
+        href,
+    } = props;
+    const { value: title } = children[0];
+
+    return (
+        <a href={href} target='_blank'>
+            {title}
+        </a>
+    );
+};
